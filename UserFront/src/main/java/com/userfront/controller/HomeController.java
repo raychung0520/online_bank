4b1ad58366644ef6e071cf105aa0b523d7a2b47b
Ray <raychung0520@gmail.com>
2020-06-18 08:42:20 -0700

    Created classes and html with styling

---
+++ UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -0,0 +1,41 @@
+package com.userfront.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.userfront.domain.User;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup(Model model) {
+		
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		return "signup";
+	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public void signupPost(Model model) {
+//		
+//		if(userService.checkEmailExists(user.getEmail())) {
+//			
+//		}
+	}
+
+
+}
